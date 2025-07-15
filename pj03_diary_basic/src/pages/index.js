2fb9a6d0ecbd569a88a63fc56e9212f4ce648df7
jinmi <jzmm00@gmail.com>
2025-07-15 14:45:45 +0900

    pages/index 저장 누락

--- pj03_diary_basic/src/pages/index.js
+++ pj03_diary_basic/src/pages/index.js
@@ -1,11 +1,9 @@
-import HomePage from "./HomePage";
 import ListPage from "./ListPage";
 import CreatePage from "./CreatePage";
 import EditPage from "./EditPage";
 import DetailPage from "./DetailPage";
 
 export{
-    HomePage,
     ListPage,
     CreatePage,
     EditPage,
